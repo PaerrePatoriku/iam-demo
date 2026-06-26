3e86606d4d0f5cca3db240522bff52bcf65ac8a9
Patrik Kumpula <patrik.kump@gmail.com>
2026-06-27 01:52:21 +0300

    separate concerns of backend router to a core router file

--- iam-backend/index.ts
+++ iam-backend/index.ts
@@ -1,13 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import { useRouter } from './routes/core';
 dotenv.config({ path : `.env` })
 
 const app : express.Application = express();
 const port = process.env.NODE_APPLICATION_LISTEN_PORT;
 
-app.get("/", (req, res) => {
-    res.send("GET Response")
-})
+app.use("/api",useRouter())
 
 app.listen(port, () => {
     console.log(`Started backend on the port ${port}!`)
