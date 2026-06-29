e6d28ef3a5157ccf102298e858a5775f45c18c9b
Patrik Kumpula <patrik.kump@gmail.com>
2026-06-30 01:15:44 +0300

    commenting

--- iam-backend/middleware/userAttachment.ts
+++ iam-backend/middleware/userAttachment.ts
@@ -2,6 +2,11 @@ import { Request, Response, NextFunction } from "express";
 
 const attachUser = (req : Request,  res : Response, next : NextFunction) => {
     const token = req.auth;
+    /*
+        The idea: attaching an "user" to the request allows us to reject or allow the request
+        using our own signature, rather than whatever syntax keycloak or some other system does.
+        The keycloak jwt data is transformed into an internal type, that then can be used to allow or deny requests and transmit other data.
+    */
     req.user = {
         id : token?.sub,
         username : token?.preferred_username,
